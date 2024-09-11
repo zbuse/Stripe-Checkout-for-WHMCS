de78f37c92181239778a45492f1a95a696b4b969
zbuse <5518208+zbuse@users.noreply.github.com>
2024-09-12 01:24:05 +0800

    Update webhooks.php

--- modules/gateways/stripecheckout/webhooks.php
+++ modules/gateways/stripecheckout/webhooks.php
@@ -1,6 +1,7 @@
 <?php
 use Stripe\StripeClient;
 use Stripe\Webhook;
+use WHMCS\Database\Capsule;
 
 require_once __DIR__ . '/../../../init.php';
 require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
@@ -41,31 +42,31 @@ try {
     if ($event->type == 'checkout.session.completed') {
         $stripe = new StripeClient($gatewayParams['StripeSkLive']);
         $checkoutId = $event->data->object->id;
-        $checkoutsessions = $stripe->checkout->sessions->retrieve($checkoutId,[]);
+        $checkout = $stripe->checkout->sessions->retrieve($checkoutId,[]);
 		
-        if ($checkoutsessions['payment_status'] == 'paid' && $checkoutsessions['status'] == 'complete') {
-            $invoiceId = checkCbInvoiceID($checkoutsessions['metadata']['invoice_id'], $paymentmethod);
-            $paymentId = $session->payment_intent;
-	checkCbTransID($paymentId);
+        if ( $checkout->payment_status == 'paid' || $checkout->status == 'complete') {
+            $invoiceId = checkCbInvoiceID($checkout['metadata']['invoice_id'], $paymentmethod);
+            $paymentId = $checkout->payment_intent;
+		checkCbTransID($paymentId);
 
         //Get Transactions fee
-        $paymentIntent = $stripe->paymentIntents->retrieve($checkoutsessions->payment_intent, []);
+        $paymentIntent = $stripe->paymentIntents->retrieve($checkout->payment_intent, []);
         $charge = $stripe->charges->retrieve($paymentIntent->latest_charge, []);
         $balanceTransaction = $stripe->balanceTransactions->retrieve($charge->balance_transaction, []);
         $fee = $balanceTransaction->fee / 100.00;
-		$invoice = Capsule::table('tblinvoices')->where('id', $invoiceId)->first();  //获取账单信息和用户 id
+		$invoice = Capsule::table('tblinvoices')->where('id', $invoiceid)->first();  //获取账单信息和用户 id
 		$currency = getCurrency( $invoice->userid ); //获取用户使用货币信息
 		
 if ( strtoupper($currency['code'])  != strtoupper($balanceTransaction->currency )) {
         $feeexchange = stripecheckout_exchange_exchange(strtoupper($balanceTransaction->currency),$currency['code']);
         $fee = floor($balanceTransaction->fee * $feeexchange / 100.00);
 }
-            logTransaction($paymentmethod, checkoutsessions , 'stripecheckout: Callback successful');
-            addInvoicePayment( $invoiceId,$paymentId, $checkoutsessions['metadata']['original_amount'] , $fee, $paymentmethod);
-            echo json_encode( ['status'=>$checkoutsessions->payment_status] );
+            logTransaction($paymentmethod, $checkout , 'stripecheckout: Callback successful');
+            addInvoicePayment( $invoiceId,$paymentId, $checkout['metadata']['original_amount'] , $fee, $paymentmethod);
+            echo json_encode( ['status'=>$checkout->payment_status] );
             http_response_code(200);
         } else {
-            echo json_encode( ['status'=>$checkoutsessions->payment_status] );
+            echo json_encode( ['status'=>$checkout->payment_status] );
 			http_response_code(400);
 		}
     }
@@ -75,5 +76,3 @@ if ( strtoupper($currency['code'])  != strtoupper($balanceTransaction->currency 
     http_response_code(400);
     echo $e;
 }
-
-
