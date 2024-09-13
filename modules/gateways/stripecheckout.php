beb0f894b81954ba15fcb0fdb041ea135baee4a0
zbuse <5518208+zbuse@users.noreply.github.com>
2024-09-13 18:21:38 +0800

    Update stripecheckout.php

--- modules/gateways/stripecheckout.php
+++ modules/gateways/stripecheckout.php
@@ -5,59 +5,59 @@ if (!defined("WHMCS")) {
 }
 function stripecheckout_MetaData() {
 	return array(
-	        'DisplayName' => 'Stripe Checkout',
-	        'APIVersion' => '1.1', // Use API Version 1.1
+		        'DisplayName' => 'Stripe Checkout',
+		        'APIVersion' => '1.1', // Use API Version 1.1
 	'DisableLocalCreditCardInput' => true,
-	        'TokenisedStorage' => false,
-	    );
+		        'TokenisedStorage' => false,
+		    );
 }
 function stripecheckout_config($params) {
 	return array(
-	        'FriendlyName' => array(
-	            'Type' => 'System',
-	            'Value' => 'Stripe Alipay',
-		),
-	        'StripeSkLive' => array(
-	            'FriendlyName' => 'SK_LIVE 密钥',
-	            'Type' => 'text',
-	            'Size' => 30,
-	            'Description' => '填写从Stripe获取到的密钥（SK_LIVE）',
-	        ),
-	        'StripePkLive' => array(
-	            'FriendlyName' => 'PK_LIVE 密钥',
-	            'Type' => 'text',
-	            'Size' => 30,
-	            'Description' => '填写从Stripe获取到的密钥（PK_LIVE）',
-	        ),
-	        'StripeWebhookKey' => array(
-	            'FriendlyName' => 'Webhook 密钥',
-	            'Type' => 'text',
-	            'Size' => 30,
-	      'Description' => "<br> <div class='alert alert-success' role='alert' style='margin-bottom: 0px;'>Webhook设置 <a href='https://dashboard.stripe.com/webhooks' target='_blank'><span class='glyphicon glyphicon-new-window'></span> Stripe webhooks</a> 侦听的事件:payment_intent.succeeded <br>
+		        'FriendlyName' => array(
+		            'Type' => 'System',
+		            'Value' => 'Stripe Alipay',
+			),
+		        'StripeSkLive' => array(
+		            'FriendlyName' => 'SK_LIVE 密钥',
+		            'Type' => 'text',
+		            'Size' => 30,
+		            'Description' => '填写从Stripe获取到的密钥（SK_LIVE）',
+		        ),
+		        'StripePkLive' => array(
+		            'FriendlyName' => 'PK_LIVE 密钥',
+		            'Type' => 'text',
+		            'Size' => 30,
+		            'Description' => '填写从Stripe获取到的密钥（PK_LIVE）',
+		        ),
+		        'StripeWebhookKey' => array(
+		            'FriendlyName' => 'Webhook 密钥',
+		            'Type' => 'text',
+		            'Size' => 30,
+		      'Description' => "<br> <div class='alert alert-success' role='alert' style='margin-bottom: 0px;'>Webhook设置 <a href='https://dashboard.stripe.com/webhooks' target='_blank'><span class='glyphicon glyphicon-new-window'></span> Stripe webhooks</a> 侦听的事件:payment_intent.succeeded <br>
       Stripe webhook " .$params['systemurl']."modules/gateways/stripecheckout/webhooks.php
                </div><style>* {font-family: Microsoft YaHei Light , Microsoft YaHei}</style>"
-		),
-	        'StripeCurrency' => array(
-	            'FriendlyName' => '发起交易货币[默认CNY]',
-	            'Type' => 'text',
-		    "Default" => "CNY",
-		    'Size' => 30,
-	            'Description' => '默认获取WHMCS的货币，与您设置的发起交易货币进行汇率转换，再使用转换后的价格和货币向Stripe请求',
-	        ),
-	        'RefundFixed' => array(
-	            'FriendlyName' => '退款扣除固定金额',
-	            'Type' => 'text',
-	            'Size' => 30,
-	      'Default' => '0.00',
-	      'Description' => '$'
-	        ),
-	        'RefundPercent' => array(
-	            'FriendlyName' => '退款扣除百分比金额',
-	            'Type' => 'text',
-	            'Size' => 30,
-		    'Description' => "%",
-		),
-	    );
+			),
+		        'StripeCurrency' => array(
+		            'FriendlyName' => '发起交易货币[默认CNY]',
+		            'Type' => 'text',
+			    "Default" => "CNY",
+			    'Size' => 30,
+		            'Description' => '默认获取WHMCS的货币，与您设置的发起交易货币进行汇率转换，再使用转换后的价格和货币向Stripe请求',
+		        ),
+		        'RefundFixed' => array(
+		            'FriendlyName' => '退款扣除固定金额',
+		            'Type' => 'text',
+		            'Size' => 30,
+		      'Default' => '0.00',
+		      'Description' => '$'
+		        ),
+		        'RefundPercent' => array(
+		            'FriendlyName' => '退款扣除百分比金额',
+		            'Type' => 'text',
+		            'Size' => 30,
+			    'Description' => "%",
+			),
+		    );
 }
 function stripecheckout_link($params) {
 	global $_LANG;
@@ -84,17 +84,17 @@ function stripecheckout_link($params) {
 	try {
 		$paymentIntent = null;
 		$paymentIntentParams = [
-		        'amount' => $amount,
-		        'currency' => $setcurrency ,
-		        'payment_method_types' => ['card','alipay','wechat_pay',],
-		        'payment_method_types' =>$StripeMethodType ,
-			'payment_method_options' => ['card' => ['request_three_d_secure' => 'automatic',],'wechat_pay' => ['client' => 'web',]],
-		        'description' => $params['companyname'] . $_LANG['invoicenumber'] . $params['invoiceid'],
-		        'metadata' => [
-		                    'invoice_id' => $params['invoiceid'],
-		                    'original_amount' => $originalAmount
-		                ],
-		            ];
+				        'amount' => $amount,
+				        'currency' => $setcurrency ,
+				        'payment_method_types' => ['card','alipay','wechat_pay',],
+				        'payment_method_types' =>$StripeMethodType ,
+					'payment_method_options' => ['card' => ['request_three_d_secure' => 'automatic',],'wechat_pay' => ['client' => 'web',]],
+				        'description' => $params['companyname'] . $_LANG['invoicenumber'] . $params['invoiceid'],
+				        'metadata' => [
+				                    'invoice_id' => $params['invoiceid'],
+				                    'original_amount' => $originalAmount
+				                ],
+				            ];
 		//将paymentIntentId存入 session 避免多次创建交易请求
 		if (isset($_SESSION[$sessionKey])) {
 			$paymentIntentId = $_SESSION[$sessionKey];
@@ -214,27 +214,27 @@ function stripecheckout_refund($params) {
 	$amount = ($params['amount'] - $params['RefundFixed']) / ($params['RefundPercent'] / 100 + 1);
 	try {
 		$responseData = $stripe->refunds->create([
-		            'payment_intent' => $params['transid'],
-		            'amount' => $amount * 100.00,
-		            'metadata' => [
-		                'invoice_id' => $params['invoiceid'],
-		                'original_amount' => $params['amount'],
-		            ]
-		        ]);
+				            'payment_intent' => $params['transid'],
+				            'amount' => $amount * 100.00,
+				            'metadata' => [
+				                'invoice_id' => $params['invoiceid'],
+				                'original_amount' => $params['amount'],
+				            ]
+				        ]);
 		return array(
-		            'status' => ($responseData->status === 'succeeded' || $responseData->status === 'pending') ? 'success' : 'error',
-		            'rawdata' => $responseData,
-		            'transid' => $params['transid'],
-		            'fees' => $params['amount'],
-		        );
+				            'status' => ($responseData->status === 'succeeded' || $responseData->status === 'pending') ? 'success' : 'error',
+				            'rawdata' => $responseData,
+				            'transid' => $params['transid'],
+				            'fees' => $params['amount'],
+				        );
 	}
 	catch (Exception $e) {
 		return array(
-		            'status' => 'error',
-		            'rawdata' => $e->getMessage(),
-		            'transid' => $params['transid'],
-		            'fees' => $params['amount'],
-		        );
+				            'status' => 'error',
+				            'rawdata' => $e->getMessage(),
+				            'transid' => $params['transid'],
+				            'fees' => $params['amount'],
+				        );
 	}
 }
 function stripecheckout_exchange($from, $to) {
